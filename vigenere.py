a0c0add69a7985c4e616c6a633af9ede1ff4e014
unknown <anonuevoloraine6@gmail.com>
2023-04-17 22:23:03 +0800

    header and user's name

---
+++ vigenere.py
@@ -0,0 +1,13 @@
+#Anonuevo, Loraine N.
+#BSCPE 1-4
+#Problem 3
+
+#Header
+print("\033[;33;1;3m\033[10m" * 65)
+print("\033[;33;1;3mAyooo! It's a pleasure to have you here!\033[0m".center(81))
+print("\033[;33;1;3mಠ\033[10m" * 65)
+
+#Ask the user's name 
+user_name = input(f"\n\033[;45;1;3mWhat is your name? \033[0m")
+print(f"\n\033[;1;3mNice to meet you, \033[;100;1;3m" + user_name +"\033[;1;3m!\033[0m")
+print("\033[0m\033[;35;1;3mKindly input the text that you want to encrypt!\033[0m")
